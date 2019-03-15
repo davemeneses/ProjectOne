99889b7f7c58f48cb49a491f747db5a9609485a2
Golden Dragon <arajchwald@gmail.com>
2019-03-15 13:28:13 -0500

    yeet

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,65 +1,74 @@
-//   document.write(
+$(document).ready(function () {
+    var instance = M.Sidenav.getInstance(elem);
+    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown();
+    $('.drag-target').on('mouseover', function () {
+        instance.open();
+    })
+});
+
+//document.write(
 //     "Welcome to our visitors from " +
 //       geoplugin_region() +
 //       ", " +
 //       geoplugin_countryName()
 //   );
-var x = geoplugin_region();
-var city = "";
-function getLocation() {
-  if (x === "Illinois") {
-    //   alert("You are in " + x);
-    city = "Chicago";
-  } else if (x === "Louisiana") {
-    city = "New Orleans";
-  } else if (x === "Tennessee") {
-    city = "Nashville";
-  } else if (x === "Texas") {
-    city = "Austin";
-  } else if (x === "Georgia") {
-    city = "Atlanta";
-  } else if (x === "Detroit") {
-    city = "Michigan";
-  } else if (x === "New York") {
-    city = "New York";
-  } else if (x === "Minnesota") {
-    city = "Minneapolis";
-  } else if (x === "California") {
-    city = "Los Angeles";
-  }
-}
-getLocation();
-console.log("this is the city to plug in to api call", city);
+// var x = geoplugin_region();
+// var city = "";
+// function getLocation() {
+//   if (x === "Illinois") {
+//     //   alert("You are in " + x);
+//     city = "Chicago";
+//   } else if (x === "Louisiana") {
+//     city = "New Orleans";
+//   } else if (x === "Tennessee") {
+//     city = "Nashville";
+//   } else if (x === "Texas") {
+//     city = "Austin";
+//   } else if (x === "Georgia") {
+//     city = "Atlanta";
+//   } else if (x === "Detroit") {
+//     city = "Michigan";
+//   } else if (x === "New York") {
+//     city = "New York";
+//   } else if (x === "Minnesota") {
+//     city = "Minneapolis";
+//   } else if (x === "California") {
+//     city = "Los Angeles";
+//   }
+// }
+// getLocation();
+// console.log("this is the city to plug in to api call", city);
 
-//function for checking url sent from spotify
+// //function for checking url sent from spotify
 
-function GetURLParameter(sParam) {
-  var sPageURL = window.location.hash;
-  console.log(sPageURL);
-  var sURLVariables = sPageURL.split("&");
-  for (var i = 0; i < sURLVariables.length; i++) {
-    var sParameterName = sURLVariables[i].split("=");
-    if (sParameterName[0] == sParam) {
-      return sParameterName[1];
-    }
-  }
-}
+// function GetURLParameter(sParam) {
+//   var sPageURL = window.location.hash;
+//   console.log(sPageURL);
+//   var sURLVariables = sPageURL.split("&");
+//   for (var i = 0; i < sURLVariables.length; i++) {
+//     var sParameterName = sURLVariables[i].split("=");
+//     if (sParameterName[0] == sParam) {
+//       return sParameterName[1];
+//     }
+//   }
+// }
 
-//logs an access token sent from spotfiy
-console.log(GetURLParameter("#access_token"));
+// //logs an access token sent from spotfiy
+// console.log(GetURLParameter("#access_token"));
 
-var accessToken = GetURLParameter("#access_token");
+// var accessToken = GetURLParameter("#access_token");
 
-//ajax request using accessToken
+// //ajax request using accessToken
 
-$.ajax({
-  url: "https://api.spotify.com/v1/me",
-  headers: {
-    Authorization: "Bearer " + accessToken
-  }
-}).then(function(response) {
-  console.log(response);
-});
+// $.ajax({
+//   url: "https://api.spotify.com/v1/me",
+//   headers: {
+//     Authorization: "Bearer " + accessToken
+//   }
+// }).then(function(response) {
+//   console.log(response);
+// });
 
-var apiURL =
-  "https://accounts.spotify.com/authorize?response_type=code&client_id=89925106e2f44201b6be245abb2f7728&scope=playlist-modify-public&redirect_uri=https://davemeneses.github.io/ProjectOne/";
+// var apiURL =
+//   "https://accounts.spotify.com/authorize?response_type=code&client_id=89925106e2f44201b6be245abb2f7728&scope=playlist-modify-public&redirect_uri=https://davemeneses.github.io/ProjectOne/";
